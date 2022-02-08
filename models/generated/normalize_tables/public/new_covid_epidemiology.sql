32d88b2847eee7334dcadbfd7ab4b6b7e2fdc54e
dassolkim <kimds0926@kangwon.ac.kr>
2022-02-08 17:21:24 +0900

    type modification

--- models/generated/normalize_tables/public/new_covid_epidemiology.sql
+++ models/generated/normalize_tables/public/new_covid_epidemiology.sql
@@ -2,12 +2,13 @@
 -- Final base SQL model
 select
     {{ adapter.quote('key') }},
-    {{ adapter.quote('date') }},
-    new_tested,
+    cast({{ adapter.quote('date') }} as timestamp),
+    cat(new_tested as bigint),
     cast(new_deceased as integer),
-    total_tested,
+    cast(total_tested as bigint),
     new_confirmed,
     new_recovered
+   
   
 from {{ ref('covid_epidemiology_ab3') }}
 -- covid_epidemiology from {{ source('public', '_airbyte_raw_covid_epidemiology') }}
